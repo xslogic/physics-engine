2cffb6673e05ceb8f958e7311f29caf3d71804bf
Arun Suresh <arun.suresh@gmail.com>
2008-11-24 01:02:30 +0530

    small changes

--- makeall.sh
+++ makeall.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
-mkdir build/hidir
-mkdir build/odir
-mkdir build/bin
 
 for x in `find src -type f -depth 1 | awk -F"/" '{print $2}'`
 do   
 	PROG=`basename $x | awk -F"." '{print $1}'`
-	cd src ; ghc --make $x -hidir ../build/hidir -odir ../build/odir -o ../build/bin/$PROG; cd ..
+	PDIR=$PROG
+	rm -rf $PDIR
+	mkdir $PDIR
+	mkdir $PDIR/hidir
+	mkdir $PDIR/odir
+	mkdir $PDIR/bin
+	echo "cd src ; ghc --make $x -hidir ../$PDIR/hidir -odir ../$PDIR/odir -o ../$PDIR/bin/runme; cd .."
+	cd src ; ghc --make $x -hidir ../$PDIR/hidir -odir ../$PDIR/odir -o ../$PDIR/bin/runme; cd ..
 done
